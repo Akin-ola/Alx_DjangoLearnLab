59370bee8e09fc2109c7a1b716f86efd694b3980
Akin-ola <idris_akinola@yahoo.com>
2025-02-14 13:49:06 +0100

    Write script in create md file.

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -4,4 +4,16 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+
+    # def create(self):
+    #     return f"{self.title} creation successful."
     
+    # def filter(self):
+    #     return f"title={self.title}, author={self.author}, publication_year={self.publication_year}."
+    
+    # def update(self):
+    #     return f"{self.title} updated successful."
+    
+    # def delete(self):
+    #     return f"{self.title} deleted."
+        
