806d2e13709a69b3cd4770cfc909c83eb2ba63f6
Akin-ola <idris_akinola@yahoo.com>
2025-02-18 23:22:40 +0100

    modified models in models file.

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,6 +3,9 @@ from django.db import models
 """ Author model."""
 class Author(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
     
 
 """ Book model."""
