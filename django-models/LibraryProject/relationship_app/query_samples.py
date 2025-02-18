1be47be20bd6c5498d0fbdb87f829edafe4dc74a
Akin-ola <idris_akinola@yahoo.com>
2025-02-18 23:44:17 +0100

    modified models in models file.2

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,6 +2,6 @@ from .models import Author, Book, Library, Librarian
 
 Author.books.all()
 
-Library.objects.get(name="library_name")
+Library.objects.get(name=library_name)
 
 Librarian.objects.filter(name="library_name")
