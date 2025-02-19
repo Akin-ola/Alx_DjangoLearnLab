ee5c863bf86c45c60f8892de2045f799f1a08b0b
Akin-ola <idris_akinola@yahoo.com>
2025-02-19 21:32:18 +0100

    Modified scripts in query file4

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,6 +1,7 @@
 from .models import Author, Book, Library, Librarian
 
 Author.objects.get(name=author_name)
+Book.objects.filter(author=author)
 
 Library.objects.get(name=library_name)
 library_name.books.all()
