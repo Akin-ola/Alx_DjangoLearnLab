7cb41747a6b89647ab349a1a59a53a840303a4b1
Akin-ola <idris_akinola@yahoo.com>
2025-02-19 21:17:17 +0100

    Modified scripts in query file

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -7,22 +7,42 @@ class Author(models.Model):
     def __str__(self):
         return self.name
     
-
 """ Book model."""
 class Book(models.Model):
-    book_title = models.CharField(max_length=50)
-    author_name = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="authors")
 
 """Library model."""
 class Library(models.Model):
-    library_name = models.CharField(max_length=50)
-    book_title = models.ManyToManyField(Book, related_name="library")
+    name = models.CharField(max_length=50)
+    book = models.ManyToManyField(Book, related_name="books")
 
 """ Librarian model."""
 class Librarian(models.Model):
-    librarian_name = models.CharField(max_length=50)
-    library_name = models.OneToOneField(Library, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name="library")
 
 
+# Testing the models relationships.
+
+# author1 = Author.objects.create(name="Akin")
+# author2 = Author.objects.create(name="Tunji")
+
+# book1 = Book.objects.create(title="sea", author = author1)
+# book2 = Book.objects.create(title="rise", author = author2)
+# book3 = Book.objects.create(title="Crow", author = author2)
+
+# Book.objects.filter(author=author2)
+
+# library1=Library.objects.create(name="Yisa Doko")
+# library2=Library.objects.create(name="Ibrahim Alfa")
+
+# library1.book.add(book1)
+# library1.book.add(book2)
+
+# library1.books.all()
+
+# librarian1 = Librarian.objects.create(name="max",library=library1)
+# librarian2 = Librarian.objects.create(name="john", library=library2)
 
 # Create your models here.
--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,7 +1,7 @@
 from .models import Author, Book, Library, Librarian
 
-Author.objects.get(name=author_name)
+Book.objects.filter(author=author)
 
 Library.objects.get(name=library_name)
 
-Librarian.objects.filter(name="library_name")
+Librarian.objects.filter(library=library)
