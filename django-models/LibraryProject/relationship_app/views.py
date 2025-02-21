405e7c0206201d7e1d3845d448b09bb30647f1da
Akin-ola <idris_akinola@yahoo.com>
2025-02-21 01:46:15 +0100

    Create Views and templates 2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Library
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book
 
 # Create your views here.
 def list_books(request):
