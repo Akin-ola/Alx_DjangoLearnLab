bbdda363e46099867d4ce350a2434ebc4247c195
Akin-ola <idris_akinola@yahoo.com>
2025-02-21 01:47:59 +0100

    Create Views and templates 2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -12,7 +12,7 @@ def list_books(request):
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_details.html'
+    template_name = 'relationship_app/library_detail.html'
 
     def get_context_data(self):
         # context = super().get_context_data()
