74cb9ac998b675711812522c9be95d6bb8a0531a
Akin-ola <idris_akinola@yahoo.com>
2025-02-22 16:34:58 +0100

    task 2 done

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from .views import list_books
-
+from .views import LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path("", view=list_books, name="home"),
     path("", view=LibraryDetailView.get_context_data)
+]
+
+urlpatterns = [
+    path("", LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path("", LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout')  
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -19,3 +19,21 @@ class LibraryDetailView(DetailView):
         # context = super().get_context_data()
         context = self.get_object()
         context['librarian'] = library.get_librarian()
+
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('Login')
+    template_name = 'relationship_app/register.html'
+
+class LoginView(CreateView):
+    template_name = 'relationship_app/login.html'
+    next_page = reverse_lazy('Login')
+    form_class = AuthenticationForm
+
+class LogoutView(CreateView):
+    template_name = 'relationship_app/logout.html'
+    form_class = AuthenticationForm
