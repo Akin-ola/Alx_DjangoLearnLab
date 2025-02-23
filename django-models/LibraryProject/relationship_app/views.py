aabe7757ced6a7d7c1c1e8ecf1584e9c19cbf985
Akin-ola <idris_akinola@yahoo.com>
2025-02-23 23:21:36 +0100

    task 2 done

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -26,7 +26,7 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 class RegisterView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     success_url = reverse_lazy('Login')
     template_name = 'relationship_app/register.html'
 
