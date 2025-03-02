bf9a5845cbb3cf8cd9ff34d5a55171020d99575e
Akin-ola <idris_akinola@yahoo.com>
2025-03-02 10:01:46 +0100

    modified the app to my understanding

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -38,6 +38,6 @@ class LogoutView(LogoutView):
     """"""
 
 class User_register(CreateView):
-    form_class = UserCreationForm("relationship_app/register.html")
+    form_class = UserCreationForm()
     success_url = reverse_lazy(LoginView)
     template_name = "relationship_app/register.html"
