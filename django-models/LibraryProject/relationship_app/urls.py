1be8f1e813ee637701797c416dccf539c2eff394
Akin-ola <idris_akinola@yahoo.com>
2025-03-02 09:56:36 +0100

    modified the app to my understanding

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from .views import list_books
-from .views import User_register, User_login, User_logout
+from .views import User_register, LoginView, LogoutView
 from .views import LibraryDetailView
 
 urlpatterns = [
     path("", view=list_books, name="home"),
     path("", LibraryDetailView.as_view(), name="library_details"),
     path("", User_register.as_view(), name="views.register"),
-    path("", User_login.as_view(), name="login"),
-    path("", User_logout.as_view(), name="logout"), 
+    path("", LoginView.as_view(template_name = "relationship_app/login.html"), name="login"),
+    path("", LogoutView.as_view(template_name = "relationship_app/login.html"), name="logout"), 
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,7 @@ from .models import Book
 from django.views.generic import ListView
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
 
@@ -31,13 +31,13 @@ class LibraryDetailView(ListView):
 
 
 
-class User_login(LoginView):
-    template_name = "relationship_app/login.html"
+class LoginView(LoginView):
+    """"""
 
-class User_logout(LogoutView): 
-    template_name = "relationship_app/logout.html"
+class LogoutView(LogoutView): 
+    """"""
 
 class User_register(CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy(User_login)
+    success_url = reverse_lazy(LoginView)
     template_name = "relationship_app/register.html"
