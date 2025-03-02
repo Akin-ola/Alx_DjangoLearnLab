e4df09b4dc371f70a84f59131bcafcab94dea9d4
Akin-ola <idris_akinola@yahoo.com>
2025-03-02 09:46:53 +0100

    modified the app to my understanding

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,7 @@ from .models import Book
 from django.views.generic import ListView
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
 
