184fa64cd2da25929d78d18b012ad674518307b1
Akin-ola <idris_akinola@yahoo.com>
2025-03-16 14:21:40 +0100

    applied filter and search capability to view

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -7,4 +7,5 @@ class Author(models.Model):   # An Author model that inherits from django models
 class Book(models.Model):   # A Book model that inherits from django models
     title = models.CharField(max_length=100)
     publication_year = models.IntegerField()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, primary_key=False, on_delete=models.CASCADE)
+    search_fields = ['title','author']
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,13 +3,28 @@ from .serializers import BookSerializer
 from .models import Book
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework import filters
 
 # Create your views here.
 
 class ListView(generics.ListAPIView):
-    queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'author']
+
+    def get_queryset(self):
+        queryset = Book.objects.all()
+        title = self.request.query_params.get('title')
+        author = self.request.query_params.get('author')
+        publication_year = self.request.query_params.get('publication_year')
+        if title is not None:
+            queryset = queryset.filter(book__title=title)
+        elif author is not None:
+            queryset = queryset.filter(book__author=author)
+        elif publication_year is not None:
+            queryset = queryset.filter(book__publication_year=publication_year)
+        return queryset
 
 class DetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
