80f2a5bdb29f316bb9ae3e8e517cc44f6ed7b829
Akin-ola <idris_akinola@yahoo.com>
2025-03-16 17:53:59 +0100

    applied filter and search capability to view

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,14 +4,15 @@ from .models import Book
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework import filters
+from django_filters import rest_framework
 
 # Create your views here.
 
 class ListView(generics.ListAPIView):
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['title', 'author']
+    filter_backends = [rest_framework.DjangoFilterBackend]
+    filterset_fields = ['title', 'book__author']
 
     def get_queryset(self):
         queryset = Book.objects.all()
@@ -19,17 +20,19 @@ class ListView(generics.ListAPIView):
         author = self.request.query_params.get('author')
         publication_year = self.request.query_params.get('publication_year')
         if title is not None:
-            queryset = queryset.filter(book__title=title)
+            queryset = queryset.filter(book_title=title)
         elif author is not None:
             queryset = queryset.filter(book__author=author)
         elif publication_year is not None:
-            queryset = queryset.filter(book__publication_year=publication_year)
+            queryset = queryset.filter(book_publication_year=publication_year)
         return queryset
 
 class DetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'book__author',]
 
 class CreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
