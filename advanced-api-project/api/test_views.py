d0c1d05be6ad9362111c87d359d1c38cd91ad594
Akin-ola <idris_akinola@yahoo.com>
2025-03-17 00:18:52 +0100

    Write unit test for the views in test views file.

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
-from rest_framework import status, response, reverse
+from rest_framework import status, response.data 
+from rest_framework import reverse
 from .models import Book
 
 # Create your tests here.
@@ -12,7 +13,9 @@ class BookAPITestCase(TestCase):
         self.book_data = {'title': 'Sea', 'author': 'John Doe', 'publication_year': 2022}
         self.book = Book.objects.create(title='Habit', author='Jane Smith', publication_year=2020)
 
+    @login_required
     def test_create_book(self):
+        self.client.login
         response = self.client.post(reverse('book-list'), self.book_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
