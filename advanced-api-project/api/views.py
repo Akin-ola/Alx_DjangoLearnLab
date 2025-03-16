60d22604e52f4169ed97818f3914052295933e40
Akin-ola <idris_akinola@yahoo.com>
2025-03-16 13:06:07 +0100

    applied permission to views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,10 +2,7 @@ from django.shortcuts import render
 from .serializers import BookSerializer
 from .models import Book
 from rest_framework import generics
-from rest_framework.permissions import (
-    IsAuthenticatedOrReadOnly,
-    IsAuthenticated
-)
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 # Create your views here.
 
